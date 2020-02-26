f44e0590b70dc703f8652c05f70d7e1ef90a7997
jondeh <dehlinjd@gmail.com>
2020-02-26 14:31:21 -0700

    first commit

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -6,11 +6,12 @@ export default class TextContainer extends Component {
     this.state = {
       text: '',
     };
+    this.updateText = this.updateText.bind(this)
   }
 
   updateText(e) {
     this.setState({
-      text: e.target.value,
+      text: e.target.value
     });
   }
 
@@ -18,13 +19,15 @@ export default class TextContainer extends Component {
     return (
       <div className="textContainer">
         <textarea
-          // style={  }
+          style={  
+            {color: this.props.fontColor, fontFamily: this.props.fontFamily, fontSize: this.props.fontSize} }
+          
           onChange={this.updateText}
           value={this.state.text}
           placeholder="Start typing your thoughts here!"
           cols="90"
-          rows="30"
-        />
+          rows="30">
+        </textarea>
       </div>
     );
   }
